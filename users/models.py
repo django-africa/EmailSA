ddce74179fa74bc4de77b926f68efeda202ee8ac
sirrobot01 <akeremukhtar10@gmail.com>
2019-09-30 20:40:09 +0100

    Fix Custom User model

--- users/models.py
+++ users/models.py
@@ -3,21 +3,25 @@ from django.db import models
 from django_countries.fields import CountryField
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+from django.db.models.signals import pre_save
 
 
-def get_domain(instance):
-    return instance.email.split('@')[1]
+def get_domain(sender, instance, **kwargs):
+    if not instance.domain_name:
+        instance.domain_name = instance.email.split('@')[1]
+
 def validate_phone(value):
     if len(str(value)) > 13:
         raise ValidationError(_('Phone number cannot be longer than 13'))
 
 class CustomUser(AbstractUser):
-    domain_name = models.CharField(max_length=255, editable=False, default=get_domain)
+    domain_name = models.CharField(max_length=255, editable=False)
 
-
+pre_save.connect(get_domain, sender=CustomUser)
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     avatar = models.ImageField()
     phone = models.IntegerField(validators=[validate_phone])
     address = models.CharField(max_length=255)
     country = CountryField()
+
