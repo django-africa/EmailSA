800cdc3fc06d01f2e562d22f64421ad2b6c24b8b
sirrobot01 <akeremukhtar10@gmail.com>
2019-09-30 20:29:31 +0100

    Fix Custom User model

--- users/models.py
+++ users/models.py
@@ -1,45 +1,23 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django_countries.fields import CountryField
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
-class UserManager(BaseUserManager):
+def get_domain(instance):
+    return instance.email.split('@')[1]
+def validate_phone(value):
+    if len(str(value)) > 13:
+        raise ValidationError(_('Phone number cannot be longer than 13'))
 
-    def create_user(self, username, password=None, **extra_fields):
-        """Creates and saves a new user"""
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
+class CustomUser(AbstractUser):
+    domain_name = models.CharField(max_length=255, editable=False, default=get_domain)
 
-        return user
-
-    def create_superuser(self, username, password):
-        """Creates and saves a new super user"""
-        user = self.create_user(username, password)
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-
-        return user
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=7, unique=True)
-    name = models.CharField(max_length=255)
-    domain_name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'username'
-
-    class Meta:
-        db_table = 'users'
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     avatar = models.ImageField()
-    phone = models.IntegerField()
+    phone = models.IntegerField(validators=[validate_phone])
     address = models.CharField(max_length=255)
     country = CountryField()
