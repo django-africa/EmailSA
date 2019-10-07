af58e3e0d886af955c56e1a951d6ebcd81ae4695
marvelike <marvelike7@gmail.com>
2019-10-07 15:51:01 +0100

    test for views updated

--- newsletter/models.py
+++ newsletter/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Header(models.Model):
     email_from = models.EmailField()
     email_title = models.CharField(max_length=250)
-    
 
     def __str__(self):
         return self.email_title
--- newsletter/tests/test_views.py
+++ newsletter/tests/test_views.py
@@ -1,5 +1,20 @@
-from django.http import HttpRequest
-from django.test import TestCase
-from django.urls import reverse
-from  newsletter import views
+from django.test import TestCase, Client
+
+
+class HeaderViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_api_header(self):
+        response = self.client.get('')
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_api_content(self):
+        response = self.client.get('/content/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_get_api_footer(self):
+        response = self.client.get('/footer/')
+        self.assertEquals(response.status_code, 200)
+
 
