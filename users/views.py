289e5db88c82be92e34823f2100100ee0df65c4f
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2019-10-13 23:05:05 +0100

    fix signup loop

--- users/views.py
+++ users/views.py
@@ -5,15 +5,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('user')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('user')
+        else:
+            form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
     else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+        return render(request, 'nosignup.html')
