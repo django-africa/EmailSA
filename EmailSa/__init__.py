eeb62e529730b561f88b1f90c74c82a3bb4ac314
okayjosh <19627177+OkayJosh@users.noreply.github.com>
2019-10-15 17:42:59 +0100

    auth refix

--- EmailSa/__init__.py
+++ EmailSa/__init__.py
@@ -1,14 +1,2 @@
 import django
 django.setup()
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-from rest_framework.authtoken.models import Token
-from django.conf import settings
-
-
-
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-    if created:
-        Token.objects.create(user=request.user)
